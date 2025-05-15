55dafa239ab5bd8c94b8ab3fa72a0bc24ee90f22
surenfernand <141896367+surenfernand@users.noreply.github.com>
2025-05-15 16:35:45 +0530

    Automated commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ attempts = 0
 print("🎯 Welcome to 'Guess the Number'!")
 print("I'm thinking of a number between {min_val} and {max_val}.")
 
-while True: test
+while True:
     try:
         guess = int(input("Take a guess: "))
         attempts += 1
